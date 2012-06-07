b0e295e96be77c52e9c3dfc5a088f11031f8282b
Elene Trull <endahner@ncsu.edu>
2012-06-06 20:45:14 -0400

    Rails new without test unit

---
+++ README.txt
@@ -0,0 +1,20 @@
+Team:
+Elene Trull     endahner
+Sudha Sriram    ssriram
+Neelam Chitnis  ncchitni
+
+Test Framework:
+RSpec
+
+Starting URL:
+local:  http://127.0.0.1:3000
+heroku:
+
+Commands to start application:
+cd to the root of the application
+run: rails s
+Browse to localhost:3000
+
+Information and credential needed for testing
+
+Other
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Program1::Application.config.secret_token = '52e0fda87b6047321b9abebbfc896df11e557901c00d6304c76b8089d1a627391efb5ffba1f1a0e45edbaf51fcee46f41b0ddbbfc87d00322ef5feee440daeeb'
